c67ed62211f3edeeec9a87b004f9a8fb3a14f4ad
MvKro <karolinamv05@gmail.com>
2015-03-06 15:54:17 -0600

    cursos

--- js/appCurso.js
+++ js/appCurso.js
@@ -1,17 +1,17 @@
 (function () { // define funcionalidad
-  var app = angular.module('carreras', ["ui.router"]);
+  var app = angular.module('cursos', ["ui.router"]);
 
-  app.directive('carrerasTabla',function ($http) {
+  app.directive('cursosTabla',function ($http) {
     return {
       restrict: 'E',
-      templateUrl: 'templates/partials/carrerasTabla.html',
+      templateUrl: 'templates/partials/cursosTabla.html',
       controller: ['$scope','$http',function ($scope,$http) {
       $scope.usuarios = [];
         $http.get('json/carreras.json').success(function (data) {
           $scope.carreras = data;
         });
       }],
-      controllerAs: 'carreras'
+      controllerAs: 'cursos'
     };
   });
 
