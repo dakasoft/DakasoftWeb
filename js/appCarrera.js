d69a5af734dce1ae2cb321f141042bd0a4bedc63
MvKro <karolinamv05@gmail.com>
2015-03-07 17:04:28 -0600

    carreras

--- js/appCarrera.js
+++ js/appCarrera.js
@@ -5,15 +5,62 @@
     return {
       restrict: 'E',
       templateUrl: 'templates/partials/carrerasTabla.html',
-      controller: ['$scope','$http',function ($scope,$http) {
-      $scope.usuarios = [];
+      controller: ['$scope','$http','ngTableParams',function ($scope,$http,ngTableParams) {
+        $scope.carrera = [];
+        $scope.editableC = "";
         $http.get('json/carreras.json').success(function (data) {
           $scope.carreras = data;
         });
-        
+         $scope.editar = function(carrera){
+          $scope.editableC = carrera;
+          $scope.nombre = carrera.nombre;
+          $scope.codigo = carrera.cod;
+        };
+
+        $scope.borrar = function(carrera){
+          
+          angular.forEach($scope.carreras, function(value, key) {
+            if(value.id == carrera.id){
+               console.log(value.id);
+              $scope.carreras.splice(key, 1);
+            }
+          });
+        };
+          $scope.agregar = function(){
+          $scope.editableC = "";
+          $scope.nombre = "";
+          $scope.codigo = ""; 
+        };
+
+           $scope.guardar = function(){
+          if($scope.editableC != ""){
+            $scope.editableC.nombre = $scope.nombre;
+            $scope.editableC.cod = $scope.codigo;
+          }else{
+            var lastUser = $scope.carreras[$scope.carreras.length - 1];
+            var newId =  lastUser.id+1;
+            /* each temporal para mostrar rol*/
+            $scope.carreras.push({id:newId,nombre: $scope.nombre,cod:$scope.codigo
+            });
+          }
+        $("#editModal").modal('hide');
+        };
       }],
       controllerAs: 'carreras'
     };
   });
+
+    app.directive('modalc',function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/modalCarreras.html',
+      controller: ['$scope','$http',function ($scope,$http) {
+       
+        
+      }],
+        controllerAs: 'modalC'
+    };
+});
+
 
 })();
