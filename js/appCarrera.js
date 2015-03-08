78f264ef35bdda7c6ad2bf97eb65c65f636423c9
yureii00 <aronchj7@live.com>
2015-03-08 12:25:54 -0600

    fix por aaron

--- js/appCarrera.js
+++ js/appCarrera.js
@@ -22,6 +22,7 @@
           $scope.editableC = carrera;
           $scope.nombre = carrera.nombre;
           $scope.codigo = carrera.cod;
+          $scope.cursosSeleccionados = angular.copy(carrera.cursos);
         };
 
         $scope.borrar = function(carrera){
@@ -33,7 +34,8 @@
             }
           });
         };
-          $scope.borrarCurso = function(curso){
+
+        $scope.borrarCurso = function(curso){
           angular.forEach($scope.cursosSeleccionados, function(value, key) {
             if(value.id == curso.id){
                console.log(value.id);
@@ -41,30 +43,36 @@
             }
           });
         };
-          $scope.agregar = function(){
-           
+
+        $scope.agregar = function(){
+          $scope.cursosSeleccionados = [];
           $scope.editableC = "";
           $scope.nombre = "";
           $scope.codigo = ""; 
         };
-           $scope.agregarCurso = function(){
-            angular.forEach($scope.cursos, function(value, key) {
+
+        $scope.agregarCurso = function(){
+          angular.forEach($scope.cursos, function(value, key) {
             if(value.id == $scope.curso){
               $scope.curso = value.nombre;
               $scope.cursoId = value.id;
             }
           });
           $scope.cursosSeleccionados.push({id: $scope.cursoId, nombre:  $scope.curso });
-
         };
 
            $scope.guardar = function(){
           if($scope.editableC != ""){
             $scope.editableC.nombre = $scope.nombre;
             $scope.editableC.cod = $scope.codigo;
+            $scope.editableC.cursos = $scope.cursosSeleccionados;
           }else{
             var lastUser = $scope.carreras[$scope.carreras.length - 1];
-            var newId =  lastUser.id+1;
+            if(lastUser){
+              var newId =  lastUser.id+1; 
+            }else{ 
+              var newId = 1
+            }
             /* each temporal para mostrar rol*/
             $scope.carreras.push({id:newId,nombre: $scope.nombre,cod:$scope.codigo,cursos :$scope.cursosSeleccionados
             });
