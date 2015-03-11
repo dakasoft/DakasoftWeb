d2066b12c765ecef2a3468a2943d9392e5d0a71d
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-11 00:13:46 -0600

    base factor humano
    otra vez haciendose :ghost:

--- js/app_factorHumano.js
+++ js/app_factorHumano.js
@@ -1,38 +1,31 @@
 (function () { // define funcionalidad
   var app = angular.module('factorHumano', ["ui.router"]);
 
-  app.directive('rubricaFactorHumano',function ($http) {
+  app.directive('factorHumano',function ($http) {
     return {
       restrict: 'E',
       templateUrl: 'templates/partials/rubricaFactorHumano.html',
       controller: ['$scope','$http','ngTableParams',function ($scope,$http,ngTableParams) {
         $scope.carrera = [];
       
-        $http.get('json/carreras.json').success(function (data) {
-          $scope.carreras = data;
+        // $http.get('json/carreras.json').success(function (data) {
+        //   $scope.carreras = data;
 
-        });
+        // });
         
         
 
         
-        };
+       
       }],
       controllerAs: 'carreras'
     };
   });
 
-    app.directive('modalc',function ($http) {
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/partials/modalCarreras.html',
-      controller: ['$scope','$http',function ($scope,$http) {
+   
        
-        
-      }],
-        controllerAs: 'modalC'
-    };
-});
+  
+
 
 
 })();
