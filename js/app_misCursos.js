28d0808bf5542452ab20e25d6a1b4167a2cdd88d
ldmv0804 <luisdanielmv@gmail.com>
2015-03-11 01:27:50 -0600

    WIP

--- js/app_misCursos.js
+++ js/app_misCursos.js
@@ -2,40 +2,48 @@
   var app = angular.module('misCursos', ["ui.router"]);
 
 
-app.controller('misCursos', ['$scope','$http', function($scope,$http) {
+  app.controller('misCursos', ['$scope','$http', function($scope,$http) {
+    $scope.estudianteActual = {};
+    $http.get('json/estudiantes.json').success(function (data) {
+      $scope.estudianteActual = data;
+      console.log($scope.estudianteActual);
+     });
 
+    $scope.guardarInfoEquipo = function(curso){
+    	//logica para guardar datos de equipo
+    	console.log(curso);
+    }
 
-  $scope.estudianteActual={};
-
-  $http.get('json/estudiantes.json').success(function (data) {
-    $scope.estudianteActual = data;
-    console.log($scope.estudianteActual);
-   });
-
-  $scope.guardarInfoEquipo = function(curso){
-  	//logica para guardar datos de equipo
-  	console.log(curso);
-  }
-
-  $scope.toogleInfo = function(estado){
-  	if(estado)
-  	{
-  		return false;
-  	}
-  	return true;
-  }
+    $scope.toogleInfo = function(estado){
+    	if(estado)
+    	{
+    		return false;
+    	}
+    	return true;
+    }
 
 }]);
 
-app.directive('navCursos',function ($http) {
-  return {
-    restrict: 'E',
-    templateUrl: 'templates/partials/navCursos.html',
-    controller: ['$scope','$http',function ($scope,$http) {
+  app.directive('navCursos',function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/navCursos.html',
+      controller: ['$scope','$http',function ($scope,$http) {
 
-    }],
-    controllerAs: 'modalCntrl'
-  };
-});
+      }],
+      controllerAs: 'modalCntrl'
+    };
+  });
+
+  app.directive('modalMiEquipo',function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/modalMiEquipo.html',
+      controller: ['$scope','$http',function ($scope,$http) {
+
+      }],
+      controllerAs: 'modalCntrl'
+    };
+  });
 
 })();
