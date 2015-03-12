da13ab510f6636863353b4b9e9a0dee24f047a7e
ldmv0804 <luisdanielmv@gmail.com>
2015-03-12 17:54:44 -0600

    Funcionalidad de los modals
    1) Me falta agregar la funcionalidad de agregar nuevos equipos (no
    guarda!!!)
    2) Me falta agregar la rubrica en estudiantes
    3) Me falta llenar el modal de agregar nuevas entregas

--- js/app_verCursos.js
+++ js/app_verCursos.js
@@ -5,30 +5,94 @@
   app.controller('verCursos', ['$rootScope','$scope','$http', function ($rootScope, $scope,$http) {
     var verCursos = this;
 
-    $scope.modalTeam = {}
-
     verCursos.bCourseDisplay = false;
     verCursos.currentView = '';
+
     $rootScope.courses = {};
+    
+    $scope.modalTeam = {};
+    $scope.modalStudent = {};
+    $scope.modalAssignment = {};
+    $scope.oEditPointer = {};
+
+    $scope.users = {};
+
+    $http.get('json/usuarios.json').success(function (data) {
+      $scope.users = data;
+    });
 
     $http.get('json/vercursos.json').success(function (data) {
       console.log(data);
       $rootScope.courses = data;
     });
 
-    $scope.showTeam = function (pTeam) {
-      $scope.modalTeam = pTeam;
+    $scope.courseDisplayToggle = function (psViewSwitch) {
+      if (psViewSwitch !== verCursos.currentView) {
+        verCursos.currentView = psViewSwitch;
+        return true;
+      } else {
+        verCursos.currentView = '';
+        return false;
+      }
+    };
+
+    $scope.newTeam = function (pGroup) {
+      $scope.oEditPointer = pGroup;
+      $scope.modalTeam.id = '';
+      $scope.modalTeam.name = '';
+      $scope.modalTeam.logo = 'img/logomovile.png';
+      $scope.modalTeam.mission = '';
+      $scope.modalTeam.vision = '';
+      $scope.modalTeam.integrantes = [];
+    };
+
+    $scope.saveNewTeam = function () {
+      var teamTemp = angular.copy($scope.modalTeam);
+      $scope.oEditPointer.push(teamTemp);
+      $scope.modalTeam = {};
+    };
+
+    $scope.editTeam = function (pTeam) {
+      $scope.oEditPointer = pTeam;
+      $scope.modalTeam = angular.copy(pTeam);
+    };
+
+    $scope.saveTeam = function () {
+      angular.forEach($scope.modalTeam, function (pValue, pKey) {
+        $scope.oEditPointer[pKey] = pValue;
+      });
+      $scope.modalTeam = {};
+    };
+
+    $scope.deleteTeam = function (pGroup, pTeam) {
+      angular.forEach(pGroup.teams, function (pValue, pKey) {
+        if(pValue.id === pTeam.id){
+          pGroup.teams.splice(pKey, 1);
+        }
+      });
     }
 
-    $scope.courseDisplayToggle = function (pView, pViewSwitch) {
-      if (pViewSwitch === verCursos.currentView) {
-        alert('potato');
-        pView = false;
-      } else {
-        verCursos.currentView = pViewSwitch;
-        pView = true;
+    $scope.addStudent = function(pStudent){
+      var bValidStudent = true;
+
+      angular.forEach($scope.modalTeam.integrantes, function (pValue, pKey) {
+        if(pValue.id === pStudent.id){
+          bValidStudent = false;
+        }
+      });
+
+      if(bValidStudent){
+        $scope.modalTeam.integrantes.push(pStudent);
       }
-    }
+    };
+
+    $scope.removeStudent = function(pStudent){
+      angular.forEach($scope.modalTeam.integrantes, function (pValue, pKey) {
+        if(pValue.id === pStudent.id){
+          $scope.modalTeam.integrantes.splice(pKey, 1);
+        }
+      });
+    };
 
   }]);
 
@@ -47,6 +111,17 @@
     return {
       restrict: 'E',
       templateUrl: 'templates/partials/modalVerEquipo.html',
+      controller: ['$scope','$http',function ($scope,$http) {
+
+      }],
+      controllerAs: 'modalCntrl'
+    };
+  });
+
+  app.directive('modalVerNuevoEquipo',function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/modalVerNuevoEquipo.html',
       controller: ['$scope','$http',function ($scope,$http) {
 
       }],
