776ba1b684d08a0f3f6c488a6baf66cfb14be201
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-13 15:28:55 -0600

    directivas
    Solucionando el erro d directivas

--- js/app_rubricaCursos.js
+++ js/app_rubricaCursos.js
@@ -1,7 +1,7 @@
 (function () { // define funcionalidad
   var app = angular.module('rubricaCursos', ["ui.router"]);
 
-  app.directive('rubricaCursosTabla',function ($http) {
+  app.directive('rubricaCursos',function ($http) {
     return {
       restrict: 'E',
       templateUrl: 'templates/partials/rubricaCursosTabla.html',
@@ -13,17 +13,29 @@
           $scope.rubricaCursos = data;
         });
 
-       $scope.agregarRubro = function(){
+        $scope.editar = function(grupoRubrica){
+          $scope.editableGrupo = grupoRubrica;
+          $scope.rubrosSeleccionados = angular.copy(grupoRubrica.rubrica);
+        };
+
+        $scope.agregarRubro = function(){
 
           var lastRubro = $scope.rubrosSeleccionados[$scope.rubrosSeleccionados.length - 1];
           var newId =  lastRubro.id+1;
+          
+          // console.log($scope.rubrosSeleccionados)
+          // console.log(lastRubro.id)
+          // if ($scope.rubrosSeleccionados === 0) {
+          //    console.log('vacio 2')
+          // }
           
           console.log("rubroNombre");
           $scope.rubrosSeleccionados.push({ id:newId,nombre: $scope.rubroNombre, valor:$scope.rubroValor });
           $scope.rubroNombre = "";
           $scope.rubroValor = "";
         };
-           $scope.eliminarRubro = function(rubro){
+
+        $scope.eliminarRubro = function(rubro){
 
           angular.forEach($scope.rubrosSeleccionados, function(value, key) {
             if(value.id == rubro.id){
@@ -33,9 +45,6 @@
 
         };
 
-       
-
-        
         $scope.guardarRubrica = function(){
           $scope.editableGrupo.rubrica = $scope.rubrosSeleccionados
           $scope.rubrosSeleccionados = [];
