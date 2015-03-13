6e8d043bfd8fee077a1248d3fc614a0fca8e5200
aaron <aronchj7@live.com>
2015-03-13 11:25:35 -0600

    terminando proyecto

--- js/app_votacionPrivada.js
+++ js/app_votacionPrivada.js
@@ -8,20 +8,87 @@
       templateUrl: 'templates/partials/proyectosVotacionPrivada.html',
       controller: ['$scope','$http',function ($scope,$http) {
         $scope.proyectosElegidos = [];
+        $scope.votar = false;
 
         $http.get('json/proyectosVotacion.json').success(function (data) {
           $scope.proyectosElegidos = data;
-             $("#owl-demo").owlCarousel({
-              autoPlay : 5000,
-              stopOnHover : true,
-              navigation:true,
-              paginationSpeed : 1000,
-              goToFirstSpeed : 2000,
-              singleItem : true,
-              autoHeight : true,
-              transitionStyle:"fade"
-            });
         }); 
+
+        $scope.seleccionar = function(proyecto){
+          $scope.video =proyecto.video;
+          $("#video").attr("src", $scope.video); /* dinamic iframe */
+          $scope.video = "";
+          //console.log($scope.video);
+        };
+
+        $scope.enviarVotacion = function(proyecto){
+          if($scope.votar){
+            alert("Ya ud voto");
+          }else{
+            $scope.votar = true;
+            angular.forEach($scope.proyectosElegidos, function(value, key) {
+              value.rate = 0
+            });
+          }
+        };
+
+        $scope.addRate = function(protecto){
+          protecto.rate+=1;
+          if(protecto.rate>10){
+            protecto.rate=0;
+          }
+        };
+
+        $scope.currentRate1 = function(proyecto){
+          if(proyecto.rate==0){
+            return "img/starVacia.png"
+          }else if(proyecto.rate==1){
+            return "img/starMedia.png"
+          }else if(proyecto.rate>=2){
+            return "img/starLlena.png"
+          }          
+        };
+
+        $scope.currentRate2 = function(proyecto){
+          if(proyecto.rate==0){
+            return "img/starVacia.png"
+          }else if(proyecto.rate==3){
+            return "img/starMedia.png"
+          }else if(proyecto.rate>=4){
+            return "img/starLlena.png"
+          }  
+        };
+
+        $scope.currentRate3 = function(proyecto){
+          if(proyecto.rate==0){
+            return "img/starVacia.png"
+          }else if(proyecto.rate==5){
+            return "img/starMedia.png"
+          }else if(proyecto.rate>=6){
+            return "img/starLlena.png"
+          }  
+        };
+
+        $scope.currentRate4 = function(proyecto){
+          if(proyecto.rate==0){
+            return "img/starVacia.png"
+          }else if(proyecto.rate==7){
+            return "img/starMedia.png"
+          }else if(proyecto.rate>=8){
+            return "img/starLlena.png"
+          }  
+        };
+
+        $scope.currentRate5 = function(proyecto){
+          if(proyecto.rate==0){
+            return "img/starVacia.png"
+          }else if(proyecto.rate==9){
+            return "img/starMedia.png"
+          }else if(proyecto.rate>=10){
+            return "img/starLlena.png"
+          }  
+        };
+
 
 
 
