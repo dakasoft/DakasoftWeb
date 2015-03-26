d4f94940c98686c489dc3fadd5e762ae6967e54a
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-26 17:29:28 -0600

    hola
    commit pruebas

--- js/app_rubricas.js
+++ js/app_rubricas.js
@@ -22,12 +22,40 @@
           //objeto scope donde meto el id de cada objeto de la rubrica
           $scope.rubrosSeleccionados = angular.copy(grupoRubrica.rubrica);
         };
-        
+
+           $scope.agregarRubro = function(){
+          if($scope.rubricaForm.$valid){
+            $scope.rubricaForm.$setUntouched(true);
+            $scope.rubricaForm.$setPristine(true);
+            $(".css-form").removeClass("ng-dirty");
+            var lastRubro = $scope.rubrosSeleccionados[$scope.rubrosSeleccionados.length - 1];
+            var newId =  (lastRubro) ? lastRubro.id + 1 : 1;
+            $scope.rubrosSeleccionados.push({ id:newId,nombre: $scope.rubroNombre, valor:$scope.rubroValor });
+            $scope.rubroNombre = "";
+            $scope.rubroValor = "";
+          }else{
+            $scope.rubricaForm.$setDirty();
+          }
+
+        };
+
+         $scope.eliminarRubro = function(rubro){
+          angular.forEach($scope.rubrosSeleccionados, function(value, key) {
+            if(value.id == rubro.id){
+              $scope.rubrosSeleccionados.splice(key, 1);
+            }
+          });
+        };
+        $scope.guardarRubrica = function(){
+          $scope.editableGrupo.rubrica = $scope.rubrosSeleccionados
+          $scope.rubrosSeleccionados = [];
+          $("#modalRubricas").modal("hide");
+        };
+
       }],
       controllerAs: 'factorH'
     };
   });
-
   // ModalDialog
  app.directive('modalRubricas',function ($http) {
     return {
@@ -39,10 +67,19 @@
     };
 });
 
+ 
+})();
+
+
+
+
+
+
+        
+
+
 
      
   
 
 
-
-})();
