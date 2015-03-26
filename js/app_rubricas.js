83b55a01e3b2943beda2500b77f1f31e37df30f0
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-26 11:20:29 -0600

    directiva y partial
    Cambio de a directiva y el partial modal rubricas

--- js/app_rubricas.js
+++ js/app_rubricas.js
@@ -12,21 +12,21 @@
         $http.get('json/rubricaFH.json').success(function (data) {
           $scope.factorHumano = data;
         });
-
-        
-
-        
-
-
-
-
-
-        
       }],
       controllerAs: 'factorH'
     };
   });
 
+  // ModalDialog
+ app.directive('modalRubricas',function ($http) {
+    return {
+      restrict: 'E',
+      templateUrl: 'templates/partials/modalRubricas.html',
+      controller: ['$scope','$http',function ($scope,$http) {     
+      }],
+        controllerAs: 'modalFactorRubricas'
+    };
+});
 
 
      
