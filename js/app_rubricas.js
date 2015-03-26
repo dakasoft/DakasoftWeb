60a8f7e48dd89818a10ce8fbbf75cbc1070f2ca7
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-26 12:35:36 -0600

    rubricas del json
    se estraen las rubricas del json

--- js/app_rubricas.js
+++ js/app_rubricas.js
@@ -7,12 +7,22 @@
       templateUrl: 'templates/partials/rubricasFactorH.html',
       controller: ['$scope','$http','ngTableParams',function ($scope,$http,ngTableParams) {
         $scope.factorHumano = [];
-        
+        $scope.rubrosSeleccionados = [];
       
       
         $http.get('json/rubricaFH.json').success(function (data) {
           $scope.factorHumano = data;
         });
+        $scope.editar = function(grupoRubrica){
+          //objeto scope para validación
+          $scope.rubricaForm.$setUntouched(true);
+          $scope.rubricaForm.$setPristine(true);
+          //objeto scope que contiene los dos objetos del array rubrica
+          $scope.editableGrupo = grupoRubrica;
+          //objeto scope donde meto el id de cada objeto de la rubrica
+          $scope.rubrosSeleccionados = angular.copy(grupoRubrica.rubrica);
+        };
+        
       }],
       controllerAs: 'factorH'
     };
