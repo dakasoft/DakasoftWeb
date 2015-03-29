f48fbcdeee4d21e04a59f441434511d50bb25edb
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-03-29 16:00:16 -0600

    tooltip títulos directiva
    Se agrega una diretiva para el tolltip en el main app, se cambia el
    título editar por rubricas.

--- js/app.js
+++ js/app.js
@@ -260,6 +260,21 @@ app.directive('menuMobile', function(){
 	};
 });
 
+/*Directiva para el tooltip*/
+app.directive('tooltip', function(){
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            $(element).hover(function(){
+                // on mouseenter
+                $(element).tooltip('show');
+            }, function(){
+                // on mouseleave
+                $(element).tooltip('hide');
+            });
+        }
+    };
+});   
 
 
 })();
