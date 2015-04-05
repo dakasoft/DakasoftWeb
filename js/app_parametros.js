371bcf0d3e36fb83127eb7a36a49b68064d2c778
unknown <aronchj7@live.com>
2015-04-05 11:37:03 -0600

    some fixes

--- js/app_parametros.js
+++ js/app_parametros.js
@@ -1,42 +1,38 @@
 (function () { // define funcionalidad
   var app = angular.module('parametros', ["ui.router"]);
 
-     app.directive('parametros',function ($http) {
+  app.directive('parametros',function ($http) {
     return {
       restrict: 'E',
       templateUrl: 'templates/partials/parametros/parametrosP.html',
-      controller: ['$scope','$http','ngTableParams',function ($scope,$http,ngTableParams,funciones) {
-      $scope.parametros = [];
-      $scope.votacion = funciones.votacion();
+      controller: ['$scope','$http','ngTableParams','funciones',function ($scope,$http,ngTableParams,funciones) {
+        $scope.votaciones = [];
+        $scope.votacion = funciones.votacion();
 
-      $http.get('json/parametros.json').success(function (data) {
-        $scope.parametros = data;
-      }); 
-       
-   $scope.editar = function(parametro){
-          $scope.editableC = parametro;
-          $scope.FechaProyectos = parametro.FechaProyectos;
-          $scope.FechaCierre = parametro.FechaCierre;
+        $http.get('json/votaciones.json').success(function (data) {
+          $scope.votaciones = data;
+        }); 
+
+        $scope.editar = function(votacion){
+          $scope.votacion =  angular.copy(votacion);
+          //$scope.votacion.fechaProyectos = new Date(2014, 3, 12);
+          console.log($scope.votacion);
+          $scope.accion = "Editar";
         };
 
- $scope.guardar = function() {
-          
-  };
-      }],
-      controllerAs: 'parametros'
+        $scope.guardar = function() {
+
+        };
+      }]
     };
   });
+
   app.directive('modalParametros',function ($http) {
     return {
       restrict: 'E',
-      templateUrl: 'templates/partials/parametros/modalParametros.html',
-      controller: ['$scope','$http',function ($scope,$http) {
-       
-        
-      }],
-        controllerAs: 'modalParametros'
+      templateUrl: 'templates/partials/parametros/modalParametros.html'
     };
-});
+  });
 
 
 })();
