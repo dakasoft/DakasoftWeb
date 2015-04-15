a514e77527508c53b628056d831ab375b3c1500f
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-04-14 20:01:45 -0600

    Ànálisis
    Repaso y comentado

--- js/app_factorHumano.js
+++ js/app_factorHumano.js
@@ -8,60 +8,60 @@
       controller: ['$scope','$http','ngTableParams','funciones',function ($scope,$http,ngTableParams,funciones) {
         $scope.grupos = [];
         $scope.rubrosSeleccionados = [];
-        $scope.rubro = funciones.rubro();
+        // $scope.rubro = funciones.rubro();
 
-        $http.get('json/grupos.json').success(function (data) {
-          $scope.grupos = data;
-        });
+        // $http.get('json/grupos.json').success(function (data) {
+        //   $scope.grupos = data;
+        // });
 
-        $scope.editar = function(grupo){
-          funciones.closeC();
-          $scope.rubro = funciones.rubro();
-          $scope.grupo =  angular.copy(grupo);
-          $scope.accion = "Editar";
-        };
+        // $scope.editar = function(grupo){
+        //   funciones.closeC();
+        //   $scope.rubro = funciones.rubro();
+        //   $scope.grupo =  angular.copy(grupo);
+        //   $scope.accion = "Editar";
+        // };
 
-        $scope.guardar = function(grupo){
-          $scope.grupos = funciones.editarDeLista($scope.grupos,grupo);
-          funciones.alert("contentbody","success",'<strong>'+"Bien!.."+'</strong> guardado con exito',3500);
-          setTimeout(function(){$("#modalRubrica").modal('hide')},1000);  
-        };
+        // $scope.guardar = function(grupo){
+        //   $scope.grupos = funciones.editarDeLista($scope.grupos,grupo);
+        //   funciones.alert("contentbody","success",'<strong>'+"Bien!.."+'</strong> guardado con exito',3500);
+        //   setTimeout(function(){$("#modalRubrica").modal('hide')},1000);  
+        // };
 
-        $scope.agregarRubro = function(rubro){
-          if($scope.rubricaForm.$valid){
-            $scope.rubro = funciones.rubro();
-            var newRubro = angular.copy(rubro);
-            funciones.agregarAListaNoRepetidoPorNombre($scope.grupo.rubricaFactor,newRubro);
-            funciones.closeC();
-          }else{
-            funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> Debes llenar todos los campos',3500);
-          }
+        // $scope.agregarRubro = function(rubro){
+        //   if($scope.rubricaForm.$valid){
+        //     $scope.rubro = funciones.rubro();
+        //     var newRubro = angular.copy(rubro);
+        //     funciones.agregarAListaNoRepetidoPorNombre($scope.grupo.rubricaFactor,newRubro);
+        //     funciones.closeC();
+        //   }else{
+        //     funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> Debes llenar todos los campos',3500);
+        //   }
 
-        };
+        // };
 
-        $scope.eliminarRubro = function(rubro){
-          funciones.borrarDeListaPorNombre($scope.grupo.rubricaFactor,rubro);
-        };
+        // $scope.eliminarRubro = function(rubro){
+        //   funciones.borrarDeListaPorNombre($scope.grupo.rubricaFactor,rubro);
+        // };
 
-        function esValidoMaxValorRubro($scope){
-          var suma = 0;
-          var esValido = true;
-          var arrayRubros = $scope.rubrosSeleccionados;
-          var nuevoValor = $scope.rubroValor;
+        // function esValidoMaxValorRubro($scope){
+        //   var suma = 0;
+        //   var esValido = true;
+        //   var arrayRubros = $scope.rubrosSeleccionados;
+        //   var nuevoValor = $scope.rubroValor;
 
-          for(var i = 0; i < arrayRubros.length; i++){
-            suma +=  parseInt(arrayRubros[i].valor);
-          }
-          suma +=  parseInt(nuevoValor);
+        //   for(var i = 0; i < arrayRubros.length; i++){
+        //     suma +=  parseInt(arrayRubros[i].valor);
+        //   }
+        //   suma +=  parseInt(nuevoValor);
           
-          if(suma > 100){
-            esValido = false;
-            funciones.closeC(); 
-            funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> La suma de los valores no debe ser mayor a 100',3500);
-          }
+        //   if(suma > 100){
+        //     esValido = false;
+        //     funciones.closeC(); 
+        //     funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> La suma de los valores no debe ser mayor a 100',3500);
+        //   }
 
-          return esValido;
-        }
+        //   return esValido;
+        // }
         
       }]
     };
@@ -69,12 +69,12 @@
 
 
   // ModalDialog
-  app.directive('modalRubrica',function ($http) {
-    return {
-      restrict: 'E',
-      templateUrl: 'templates/partials/factorHumano/modalRubrica.html'
-    };
-});
+//   app.directive('modalRubrica',function ($http) {
+//     return {
+//       restrict: 'E',
+//       templateUrl: 'templates/partials/factorHumano/modalRubrica.html'
+//     };
+// });
      
 
 
