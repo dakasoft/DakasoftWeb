8a41b5884cb7f37ce7252dc3622b357fce7e1601
ldmv0804 <luisdanielmv@gmail.com>
2015-04-17 00:01:42 -0600

    DONE!!

--- php/borrarAreasAcademicas.php
+++ php/borrarAreasAcademicas.php
@@ -5,14 +5,12 @@ $data = file_get_contents("php://input");
 
 $objData = json_decode($data);
 $data = $objData->data;
-$query = "CALL areasAcademicasCrear('$data->Nombre','$data->Codigo')";
 
+$query = "CALL areasAcademicasEliminar('$data->id')";
 $result = mysqli_query($conexion,$query);
 
 if($result){
-	$Insert_Id = $result->fetch_array(MYSQLI_ASSOC);
-	echo json_encode($Insert_Id);
-	mysqli_free_result($result);
+	echo true;
 }else{
 	echo false;
 }
