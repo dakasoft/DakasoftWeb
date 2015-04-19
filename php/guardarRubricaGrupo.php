d36e3fdf72556e4e2a2dbf700f7369230952cb6d
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-04-18 19:04:58 -0600

    guardar
    Guardar rubros

---
+++ php/guardarRubricaGrupo.php
@@ -0,0 +1,17 @@
+<!-- guardarRubricaGrupo -->
+<?php
+include "conexion.php";
+$data = file_get_contents("php://input");
+
+// $objData = json_decode($data);
+// $data = $objData->data;
+
+var_dump($data);
+
+
+
+
+
+
+
+?>
---
+++ php/guardarRubrosRubricaFH.php
@@ -0,0 +1,15 @@
+<?php
+include "conexion.php";
+$data = file_get_contents("php://input");
+
+$objData = json_decode($data);
+$data = $objData->data;
+
+$qry = "CALL rubricaPorRubro('$data->rubrica','$data->id')";
+$result = mysqli_query($conexion,$qry);
+
+mysqli_close($conexion);
+
+?>
+
+
