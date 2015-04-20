0164ed013dfa004aefbfdfd5507efab6887cc961
Crolina Azofeifa Chinchilla <epic-arol87@hotmail.com>
2015-04-19 19:15:48 -0600

    cursos html
    Se listaron los cursos en el html

--- js/app_rubricaCursos.js
+++ js/app_rubricaCursos.js
@@ -25,81 +25,81 @@
           });
 
 
-        $scope.editar = function(grupoRubrica){
-           funciones.closeC();
-            $scope.rubroNombre = "";
-             $scope.rubroValor = "";
-          $scope.rubricaCursoForm.$setUntouched(true);
-          $scope.rubricaCursoForm.$setPristine(true);
-          $scope.editableGrupo = grupoRubrica;
-          $scope.rubrosSeleccionados = angular.copy(grupoRubrica.rubrica);
-        };
+        // $scope.editar = function(grupoRubrica){
+        // funciones.closeC();
+        //   $scope.rubroNombre = "";
+        //   $scope.rubroValor = "";
+        //   $scope.rubricaCursoForm.$setUntouched(true);
+        //   $scope.rubricaCursoForm.$setPristine(true);
+        //   $scope.editableGrupo = grupoRubrica;
+        //   $scope.rubrosSeleccionados = angular.copy(grupoRubrica.rubrica);
+        // };
 
-        $scope.agregarRubro = function(){
-        if($scope.rubricaCursoForm.$valid){
-          //Si ésta función lo que valida está bien agrege la nueva fila.
-          //La función valida que la suma no sea mayor de 100
-          //El scope tiene el array y valor que se va a meter
-          if(esValidoMaxValorRubro($scope)){
+        // $scope.agregarRubro = function(){
+        // if($scope.rubricaCursoForm.$valid){
+        //   //Si ésta función lo que valida está bien agrege la nueva fila.
+        //   //La función valida que la suma no sea mayor de 100
+        //   //El scope tiene el array y valor que se va a meter
+        //   if(esValidoMaxValorRubro($scope)){
 
-            $scope.rubricaCursoForm.$setUntouched(true);
-            $scope.rubricaCursoForm.$setPristine(true);
+        //     $scope.rubricaCursoForm.$setUntouched(true);
+        //     $scope.rubricaCursoForm.$setPristine(true);
 
-            var lastRubro = $scope.rubrosSeleccionados[$scope.rubrosSeleccionados.length - 1];
-            var newId = 1;
-            if(lastRubro != null){
-               newId = lastRubro.id + 1;
-            }
-            $scope.rubrosSeleccionados.push({ id:newId,nombre: $scope.rubroNombre, valor:$scope.rubroValor });
-            funciones.closeC();
-            funciones.alert("contentbody","success",'<strong>'+"Bien!.."+'</strong> guardado con exito',3500);
-            $scope.rubroNombre = "";
-            $scope.rubroValor = "";
+        //     var lastRubro = $scope.rubrosSeleccionados[$scope.rubrosSeleccionados.length - 1];
+        //     var newId = 1;
+        //     if(lastRubro != null){
+        //        newId = lastRubro.id + 1;
+        //     }
+        //     $scope.rubrosSeleccionados.push({ id:newId,nombre: $scope.rubroNombre, valor:$scope.rubroValor });
+        //     funciones.closeC();
+        //     funciones.alert("contentbody","success",'<strong>'+"Bien!.."+'</strong> guardado con exito',3500);
+        //     $scope.rubroNombre = "";
+        //     $scope.rubroValor = "";
 
-          }else{
+        //   }else{
              
-          }
+        //   }
 
-          }else{
-            funciones.closeC(); 
-            funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> Debes llenar todos los campos',3500);
-          }
-        };
+        //   }else{
+        //     funciones.closeC(); 
+        //     funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> Debes llenar todos los campos',3500);
+        //   }
+        // };
 
-        $scope.eliminarRubro = function(rubro){
+        // $scope.eliminarRubro = function(rubro){
 
-          angular.forEach($scope.rubrosSeleccionados, function(value, key) {
-            if(value.id == rubro.id){
-              $scope.rubrosSeleccionados.splice(key, 1);
-            }
-          });
+        //   angular.forEach($scope.rubrosSeleccionados, function(value, key) {
+        //     if(value.id == rubro.id){
+        //       $scope.rubrosSeleccionados.splice(key, 1);
+        //     }
+        //   });
 
-        };
+        // };
 
-        $scope.guardarRubrica = function(){
-          $scope.editableGrupo.rubrica = $scope.rubrosSeleccionados
-         setTimeout(function(){$("#modalRubrica").modal('hide')},1000);
-        };
+        // $scope.guardarRubrica = function(){
+        //   $scope.editableGrupo.rubrica = $scope.rubrosSeleccionados
+        //  setTimeout(function(){$("#modalRubrica").modal('hide')},1000);
+        // };
 
-         function esValidoMaxValorRubro($scope){
-          var suma = 0;
-          var esValido = true;
-          var arrayRubros = $scope.rubrosSeleccionados;
-          var nuevoValor = $scope.rubroValor;
+        //  function esValidoMaxValorRubro($scope){
+        //   var suma = 0;
+        //   var esValido = true;
+        //   var arrayRubros = $scope.rubrosSeleccionados;
+        //   var nuevoValor = $scope.rubroValor;
 
-          for(var i = 0; i < arrayRubros.length; i++){
-            suma +=  parseInt(arrayRubros[i].valor);
-          }
-          suma +=  parseInt(nuevoValor);
+        //   for(var i = 0; i < arrayRubros.length; i++){
+        //     suma +=  parseInt(arrayRubros[i].valor);
+        //   }
+        //   suma +=  parseInt(nuevoValor);
           
-          if(suma > 100){
-            esValido = false;
-             funciones.closeC(); 
-            funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> La suma de los valores no debe ser mayor a 100',3500);
-          }
+        //   if(suma > 100){
+        //     esValido = false;
+        //      funciones.closeC(); 
+        //     funciones.alert("contentbody","danger",'<strong>'+"Ops!.."+'</strong> La suma de los valores no debe ser mayor a 100',3500);
+        //   }
 
-          return esValido;
-        }
+        //   return esValido;
+        // }
 
       }],
       controllerAs: 'factorH'
